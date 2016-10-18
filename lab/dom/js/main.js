5505a6ee7bccceef30c662d9dda5b31de481b3cd
tammyyu <tammyyu@uw.edu>
2016-10-18 15:09:03 -0700

    Updated Fun Shapes

--- lab/dom/js/main.js
+++ lab/dom/js/main.js
@@ -6,3 +6,25 @@ var boxOne = document.getElementById('one'),
 	boxFour = document.getElementById('four'),
 	boxFive = document.getElementById('five'),
 	boxSix = document.getElementById('six');
+
+boxOne.addEventListener("click", function() {
+    boxOne.classList.add("fade-to-black");
+	boxOne.classList.add("black");
+});
+
+boxTwo.addEventListener("click", function() {
+    boxTwo.classList.add("fall");
+});
+
+boxThree.addEventListener("mouseover", function() {
+    boxThree.classList.add("fade-to-white");
+	boxThree.classList.add("white");
+	
+});
+
+boxThree.addEventListener("mouseout", function() {
+    boxThree.classList.add("fade-to-bg-color");
+	boxThree.classList.remove("white");
+});
+
+
